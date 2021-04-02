18577152f004b68c8679cb94a3bb663cebf9660e
Anthony Hevia <anthonyhevia@microsoft.com>
2021-04-02 17:28:43 -0400

    progress on creating venvs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,49 @@
-use std::fs::File;
-use std::io::{Write, BufReader, BufRead, Error};
+use std::process::{ChildStdout, Command, Stdio};
+use std::io::{BufRead, BufReader, Error, ErrorKind};
 
-struct Variable {
-    identifer: String,
-    isMut: bool
-}
-
-
-fn main() -> Result<(), Error> {
-    
-}
-
-fn create_env() -> Result<(), Error> {
+fn main() {
 
 }
 
-fn parse_file(path: &str) -> Result<(), Error> {
-    let input = File::open(path)?;
-    let buffered = BufReader::new(input);
+fn display_output(stdout: ChildStdout) -> Result<(), Error> {
+    let reader = BufReader::new(stdout);
 
-    for line in buffered.lines() {
-        for token in line.unwrap().split_whitespace() {
-            println!("{}", token);
-        }
-    }
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .for_each(|line| println!("{}", line));
+     Ok(())
+}
 
-    Ok(())
+fn create_folder(folder_name: &str) -> Result<ChildStdout, Error> {
+    let stdout = Command::new("cmd")
+        .args(&["/C", format!("mkdir {}", folder_name).as_str()])
+        .stdout(Stdio::piped())
+        .spawn()?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+
+    Ok(stdout)
+}
+
+fn create_venv(venv_name: &str) -> Result<ChildStdout, Error> {
+    let stdout = Command::new("cmd")
+        .args(&["/C", format!("python -m venv ./{}", venv_name).as_str()])
+        .stdout(Stdio::piped())
+        .spawn()?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+
+    Ok(stdout)
+}
+
+fn activate_venv(venv_name: &str, further_commands: &str) -> Result<ChildStdout, Error> {
+    let stdout = Command::new("cmd")
+        .args(&["/C", format!(".\\{}\\Scripts\\activate & {}", venv_name, further_commands).as_str()])
+        .stdout(Stdio::piped())
+        .spawn()?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+
+    Ok(stdout)
 }
